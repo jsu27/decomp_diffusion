c45852f981b9208f2548344e14429a34dc1f4c94
jsu27 <jocelin.su@gmail.com>
2023-06-05 16:03:22 -0700

    test

--- decomp_diffusion/util/dist_util.py
+++ decomp_diffusion/util/dist_util.py
@@ -32,10 +32,10 @@ def dev():
     Get the device to use for torch.distributed.
     """
     if th.cuda.is_available():
-        if 'LOCAL_RANK' in os.environ:
-            return th.device(f"cuda:{os.environ['LOCAL_RANK']}")
-        else:
-            return th.device("cuda")
+        # if 'LOCAL_RANK' in os.environ:
+        #     return th.device(f"cuda:{os.environ['LOCAL_RANK']}")
+        # else:
+        return th.device("cuda")
     return th.device("cpu")
 
 
